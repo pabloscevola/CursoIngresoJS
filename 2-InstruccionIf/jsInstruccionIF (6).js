d80b1dc875a89f5e97b759ff578bc24ed35670d6
pabloscevola <pabloscevola@users.noreply.github.com>
2019-07-15 22:31:36 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
-
-
-
+    var edad = parseInt(document.getElementById("edad").value);
+    
+    if(edad >=18){
+        alert("Es adulto")
+    }
+    else{
+        if(edad >= 13 && edad <=17){
+            alert("Es adolescente")
+        }
+        else{
+            alert("Es niño")
+        }
+    }
 
 }//FIN DE LA FUNCIÓN
