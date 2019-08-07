4cd43de43f0ed319e05a5456f975022b020dac43
pabloscevola <pabloscevola@users.noreply.github.com>
2019-08-06 22:23:10 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,44 @@
-function mostrar()
-{
+function mostrar() {
+    var marca;
+    var peso;
+    var temperatura;
+    var contarpar;
+    var bandera;
+    var maximo;
+    var marcamaximo;
+    var marca;
+    var contador;
+    var acumuladorpeso;
+
+    while (prompt("Ingrese una marca: ")) {
+        do {
+            peso = parseInt(prompt("Ingrese el peso: "));
+        } while (isNaN(peso) || peso > 100 || numero > 1);
+
+    }
+
+    if (temperatura % 2 == 0) {
+        contarpar++;
+    }
+    if (bandera) {
+        maximo = peso;
+        marcamaximo = marca;
+    }
+    else {
+        if (peso > maximo) {
+            maximo = peso;
+            marcamaximo = marca;
+        }
+    }
+
+    if (temperatura < 0) {
+        contador++;
+    }
+
+    acumuladorpeso += peso;
+    contador++;
+
+
+
 
 }
