551a2ecb6f149a55d28b67bfb19eeeacb99960b6
pabloscevola <pabloscevola@users.noreply.github.com>
2019-08-06 21:57:40 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,90 @@
-function mostrar()
-{
+function mostrar() {
+
+    var letra;
+    var numero;
+    var contadorpos = 0;
+    var contadorneg = 0;
+    var acumulador = 0;
+    var contadorpar = 0;
+    var contadorimpar = 0;
+    var contadorceros = 0;
+    var bandera;
+    var minimo;
+    var maximo;
+    var respuesta = "si";
+    var letramaxima;
+    var letraminima;
+
+    while (prompt("Ingrese un número: ")) {
+        letra = prompt("Ingrese una letra: ");
+        do {
+            numero = parseInt(prompt("Ingrese un número: "));
+        } while (isNaN(numero) || numero < 100 || numero < 100);
+    /*    do {
+            respuesta = prompt("Quiere continuar?")
+        } while (respuesta == "si");*/
+    }
+
+        if (numero % 2 == 0) { //a
+            contadorpar++;
+        }
+        else if (numero == 0) { //c
+            contadorceros++;
+        }
+        else { //b
+            contadorimpar++;
+        }
+
+        if (numero > 0) {
+            contadorpor++;
+            acumulador += numero;
+        }
+        else if (numero < 0) {
+            contadorneg++;
+            acumulador += numero;
+        }
+
+        if (bandera) {
+            bandera = false;
+            minimo = numero;
+            maximo = numero;
+            letramaxima = letra;
+            letraminima = letra;
+        }
+        else {
+            if (numero > maximo) {
+                maximo = numero;
+                letramaxima = letra;
+            }
+        }
+
+
+
+        document.write("La cantidad de números pares: " + contadorpar);
+        document.write("La cantidad de números impares: " + contadorimpar);
+        document.write("La cantidad de ceros: " + contadorceros);
+/*        
+        document.write("El promedio de todos los números positivos ingresados: " +  );
+        document.write("La suma de todos los números negativos: " +  );
+        document.write("El número y la letra del máximo y el mínimo: " +  );
+*/
+
+        /* OTRO EJERCIDIO
+        Realizar el algoritmo que pida los datos necesarios de un triángulo equilatero por promt. 
+        Validar que sea un triángulo equilatero y calcular el perímetro. 
+
+        var lado; 
+        var perimetro; 
+        var superficie; 
+
+        do {
+            lado = parseInt(prompt(" "));
+        } while (isNaN(lado) || lado < 1);
+
+        perimetro = lado * 3; 
+
+        superficie = lado * lado / 2; 
+
+        */
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,64 @@
-function mostrar()
-{
+function mostrar() {
+    /*
+        var nota;
+        var sexo;
+        var contador = 0;
+        var acumulador = 0;
+        var promedio = 0;
+    
+        nota = parseInt(prompt("Ingrese una nota entre 0 y 10."));
+        while (!(nota >= 0 && nota <= 10)) {
+            nota = parseInt(prompt("Número incorrecto. Ingrese un número entre 0 y 10."));
+        }
+    
+        sexo = prompt("Ingrese f ó m .");
+        while (!(sexo == "f" || sexo == "m")) {
+            sexo = prompt("No es sexo. Ingrese f ó m .");
+        }
+        
+        promedio = acumulador / 5
+    
+        alert("El promedio de las notas totales: " + promedio);
+        alert("La nota más baja y el sexo de esa persona: " +  );
+        alert("La cantidad de varones que su nota haya sido mayor o igual a 6: " +  );
+    */
+
+    var contador = 0;
+    var nota;
+    var sexo;
+    var acumulador = 0;
+    var minimo;
+    var sexominimo;
+
+    while (contador < 5) {
+        contador++;
+        do {
+            nota = parseInt(prompt("Ingrese una nota entre 0 y 10."));
+        } while (isNaN(nota) || (!(nota >= 0 && nota <= 10));
+
+        do {
+            sexo = prompt("Ingrese f ó m .");
+        } while (!(sexo == "f" || sexo == "m"));
+    }
+
+    acumulador = acumulador + nota
+
+    if (contador == 1) {
+        minimo = nota;
+        sexominimo = sexo;
+    }
+
+    else {
+        if (nota < minimo) {
+            minimo = nota;
+            sexominimmo = sexo;
+        }
+    }
+
+    if (sexo == m && nota >= 6) {
+        contador++
+    }
+
+
 
 }
